fc28e85f07c2c4daeb32fd9ebaa6931c9bd87967
radicalten <79894181+radicalten@users.noreply.github.com>
2025-07-13 23:41:38 -0500

    Update FrontEnd.cpp

--- source/src/FrontEnd.cpp
+++ source/src/FrontEnd.cpp
@@ -132,7 +132,7 @@ int getExtId(const char *szFilePath) {
 		pszExt++;
 		int i;
 		for (i = 0; stExtentions[i].nExtId != EXT_UNKNOWN; i++) {
-			if (!strncmp(stExtentions[i].szExt,pszExt, 8)) {
+			if (!strncmp(stExtentions[i].szExt,pszExt, 1)) {
 				return stExtentions[i].nExtId;
 			}
 		}
