bc7e9b413025f77ff34d0fc6494a354d4cf27e87
radicalten <79894181+radicalten@users.noreply.github.com>
2025-07-14 08:55:44 -0500

    Update MMU_timing.h

--- source/src/MMU_timing.h
+++ source/src/MMU_timing.h
@@ -142,8 +142,8 @@ private:
 	enum { ASSOCIATIVITY = 1 << ASSOCIATIVESHIFT };
 	enum { BLOCKSIZE = 1 << BLOCKSIZESHIFT };
 	enum { TAGSHIFT = SIZESHIFT - ASSOCIATIVESHIFT };
-	enum { TAGMASK = (~((1U << TAGSHIFT) - 1)) };
-	enum { BLOCKMASK = (~((1U << (TAGSHIFT)) - 1)) & ~((1U << BLOCKSIZESHIFT) - 1) };
+	enum { TAGMASK = (u32)(~0U << TAGSHIFT) };
+	enum { BLOCKMASK = ((u32)~0U >> (32 - TAGSHIFT)) & (u32)(~0U << BLOCKSIZESHIFT) };
 	enum { WORDSIZE = sizeof(u32) };
 	enum { WORDSPERBLOCK = (1 << BLOCKSIZESHIFT) / WORDSIZE };
 	enum { DATAPERWORD = WORDSIZE * ASSOCIATIVITY };
